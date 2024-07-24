9868e686df7d7683bdc9b809b7494df691833827
LakeshiaJ <joseph.mayaka@yahoo.com>
2024-07-24 14:54:48 +0300

    0-promise.js

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,8 @@
+function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+        resolve(0);
+    })
+}
+
+const response = getResponseFromAPI();
+console.log(response instanceof Promise);
