20ead9c7a938770907bbea92699a2dc04fb9d353
LakeshiaJ <joseph.mayaka@yahoo.com>
2024-07-24 16:48:05 +0300

    2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,5 +2,5 @@ function handleResponseFromAPI(promise) {
    return Promise
    .then({ status: 200, body: 'success'})
    .catch(new Error(''))
-   .finally('Got a response from the API')
+   .finally(console.log('Got a response from the API'))
 }
